f00f91bc09a382c780f1c99c8e5c25d5d1918a8b
masab <masabk214@gmail.com>
2023-10-26 17:37:28 -0400

    adding tilegrig

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,11 +6,12 @@ import FloatingCards from "../components/FloatingCards";
 import MediaText from "../components/MediaText";
 import AccentureNews from "../components/AccentureNews";
 import Footer from "../components/footer";
-
+import TiteGrid from '../components/tilegrid'
 function Home() {
 
     return <div>
         <Banner/>
+        <TiteGrid/>
         <Radvalue/>
         <ClientCarousel/>
         <FloatingCards/>
