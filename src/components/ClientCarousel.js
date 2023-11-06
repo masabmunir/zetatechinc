08d825456feb8df1876adcbc37773dbfcfc4c1ee
masab <masabk214@gmail.com>
2023-11-06 08:43:55 -0500

    adding css for clientCarousel

--- src/components/ClientCarousel.js
+++ src/components/ClientCarousel.js
@@ -3,10 +3,12 @@ import ExpandableCard from './expandable';
 
 const ClientCarousel = () => {
     const items = [
-        { title: 'Card 1', image: 'https://dynamicmedia.accenture.com/is/image/accenture/Accenture-Father-Daughter-Breakfast-768x768%3Arad-3x2?ts=1697180881534&dpr=off', description: 'Description for Card 1.' },
-        { title: 'Card 2', image: 'https://dynamicmedia.accenture.com/is/image/accenture/Accenture-Sustainability-Feeding-The-World%3Arad-3x2?ts=1696022628310&dpr=off', description: 'Description for Card 2.' },
-        { title: 'Card 3', image: 'https://dynamicmedia.accenture.com/is/image/accenture/Accenture-Microsoft-Cloud-Supply-Chain-Control-Tower%3Arad-3x2?ts=1696022647480&dpr=off', description: 'Description for Card 3.' },
+        { image: 'https://dynamicmedia.accenture.com/is/image/accenture/Accenture-Father-Daughter-Breakfast-768x768%3Arad-3x2?ts=1697180881534&dpr=off', description: 'As outer space gets more crowded and contested, QuSecure is protecting communications and data transmission with the agility required for the world of post-quantum computing.' },
+        { image: 'https://dynamicmedia.accenture.com/is/image/accenture/Accenture-Sustainability-Feeding-The-World%3Arad-3x2?ts=1696022628310&dpr=off', description: 'The global snacking giant is laying a tasty foundation for reinvention and growth. Accenture is helping Mondelēz International to embrace the power of data and AI, build a strong digital core and implement interoperable cloud-enabled technology.' },
+        { image: 'https://dynamicmedia.accenture.com/is/image/accenture/Accenture-Microsoft-Cloud-Supply-Chain-Control-Tower%3Arad-3x2?ts=1696022647480&dpr=off', description: 'Rapid growth of the cloud changed Microsoft’s supply chain network significantly. The solution? Data-led transformation to a supply chain control tower.' },
     ];
+
+
 
     return (
         <>
